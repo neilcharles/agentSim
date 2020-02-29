e9934afec248b2a50a74c782d86e656f9d349b27
Neil Charles <neil.charles@mediacom.com>
2020-02-29 10:06:50 +0000

    init

---
+++ R/agent.R
@@ -0,0 +1,120 @@
+#' @title Define an agent
+#'
+#' @param id An ID for the agent. Can be a number or character name.
+#' @param type Character string describing the agent's type e.g. 'predator' or 'prey'
+#' @param location_y Agent's starting y coordinate
+#' @param location_x Agent's starting x coordinate
+#'
+#' @return An 'agent' S3 object
+#' @export
+#'
+#' @examples
+#' new_agent <- agent(id = 1)
+agent <-
+  function(id = NULL,
+           type = "agent",
+           location_y = NULL,
+           location_x = NULL) {
+
+    me <- list(
+      id = id,
+      type = type,
+      location_y = location_y,
+      location_x = location_x,
+      task = function() invisible()
+    )
+
+    class(me) <- append(class(me), "agent")
+
+    return(me)
+
+  }
+
+#' Set the agent's location using x,y coordinates
+#'
+#' @param x Agent
+#' @param newLocation Location vector as a y,x (lon, lat) pair
+#'
+#' @return Agent
+#' @export
+#'
+#' @examples
+#' new_agent <- agent(id = 1, location_y = 1, location_x = 2)
+#'
+#' #Move the agent to coordinates y = 2, x = 3
+#' new_agent <- setLocation(new_agent, c(2,3))
+setLocation <- function(x, newLocation){
+  UseMethod('setLocation', x)
+}
+
+setLocation.agent <- function(x, newLocation){
+  x$location_y <- newLocation[1]
+  x$location_x <- newLocation[2]
+  x
+}
+
+#' @title Set an agent's task
+#'
+#' @description Set the task for a single agent, or for a set of agents within a population,
+#' which will be executed for each time step when the simulation is run
+#'
+#' @param x An agent or a population of agents
+#' @param task A function to be executed in each time step. You must pass \code{agent} and \code{population} as arguments to the function (see example below)
+#' @param agent_type If x is a population, which agent type(s) in the population should run this task?
+#'
+#' @return agent or population
+#' @export
+#'
+#' @examples
+#' #Build an example simulation in which each agent moves and then prints its coordinates to the console in each time step
+#'
+#' #A parameter table to define 3 agents placed at random coordinates
+#' init_pop <- tibble::tibble(
+#'   id = c(1:3),
+#'   type = "agent",
+#'   location_x = runif(3, 0, 1),
+#'   location_y = runif(3, 0, 1)
+#'   )
+#'
+#' #Define the task function, which will be run in each time step.
+#' #You must pass 'agent' and 'population' as arguments to the function
+#' #so that the agent can see itself and other agents when it runs its task.
+#' move_and_report <- function(agent, population){
+#'
+#'   #Move the agent
+#'   agent$location_y <- agent$location_y + runif(1, -0.5, 0.5)
+#'   agent$location_x <- agent$location_x + runif(1, -0.5, 0.5)
+#'
+#'   #Print the agent's position to the console
+#'   print(
+#'     paste0("Agent '", agent$id, "' position: ",
+#'     agent$location_y, ", ", agent$location_x)
+#'   )
+#' }
+#'
+#' #Create agents and assign tasks
+#' agents_with_tasks <- makePopulation(init_pop) %>%
+#'   setTask(move_and_report)
+setTask <- function(x, task, agent_type = "agent"){
+  UseMethod('setTask', x)
+}
+
+#' @export
+setTask.agent <- function(x, task){
+  x$task <- task
+
+  x
+}
+
+#' @export
+setTask.population <- function(x, task, agent_type = "agent"){
+
+  for(a in 1:length(x)){
+
+    if(x[[a]]$type %in% agent_type){
+      x[[a]]$task <- task
+    }
+  }
+
+  x
+}
---
+++ R/simulation.R
@@ -0,0 +1,148 @@
+#' Set up an agentSim simulation
+#'
+#' @param agent_population A population of agents
+#' @param time_total Number of time ticks to simulate
+#'
+#' @return a simulation
+#' @export
+#'
+#' @examples
+#' #Build an example simulation in which each agent moves and then prints its coordinates to the console in each time step
+#'
+#' #A parameter table to define 3 agents placed at random coordinates
+#' init_pop <- tibble::tibble(
+#'   id = c(1:3),
+#'   type = "agent",
+#'   location_x = runif(3, 0, 1),
+#'   location_y = runif(3, 0, 1)
+#'   )
+#'
+#' #Define the task function, which will be run in each time step.
+#' #You must pass 'agent' and 'population' as arguments to the function
+#' #so that the agent can see itself and other agents when it runs its task.
+#' move_and_report <- function(agent, population){
+#'
+#'   #Move the agent
+#'   agent$location_y <- agent$location_y + runif(1, -0.5, 0.5)
+#'   agent$location_x <- agent$location_x + runif(1, -0.5, 0.5)
+#'
+#'   #Print the agent's position to the console
+#'   print(
+#'     paste0("Agent '", agent$id, "' position: ",
+#'     agent$location_y, ", ", agent$location_x)
+#'   )
+#' }
+#'
+#' #Set up the simulation
+#' simulation <- makePopulation(init_pop) %>%
+#'   setTask(move_and_report) %>%
+#'   agentSim(10)
+agentSim <- function(agent_population = NULL,
+           time_total = NULL) {
+
+    me <- list(
+      agent_population = agent_population,
+      time_total = time_total,
+      time_cur = 1,
+      history = tibble::tibble(
+        t = seq(1:time_total),
+        log = NA
+      )
+    )
+
+    me$history$log[1] <- list(agent_population)
+
+    class(me) <- append(class(me), "simulation")
+
+    return(me)
+
+  }
+
+#' @export
+iterateSim <- function(x){
+  UseMethod("iterateSim", x)
+}
+
+#' @export
+iterateSim.simulation <- function(x){
+  if(x$time_cur < x$time_total){
+    x$history$log[x$time_cur] <- list(x$agentPopulation)
+    x$time_cur <- x$time_cur
+    return(x)
+  } else {
+    stop("You can't iterate this sim, it's already complete")
+  }
+}
+
+#' Run a simulation
+#'
+#' @param x A simulation created with agentSim()
+#' @param debug Optionally, print calculation step info to the console to aid in
+#' debugging task errors
+#'
+#' @return The original simulation, with a fully populated simulation$history$log
+#' @export
+#'
+#' @examples
+#' #' #Build an example simulation in which each agent moves and then prints its coordinates to the console in each time step
+#'
+#' #A parameter table to define 3 agents placed at random coordinates
+#' init_pop <- tibble::tibble(
+#'   id = c(1:3),
+#'   type = "agent",
+#'   location_x = runif(3, 0, 1),
+#'   location_y = runif(3, 0, 1)
+#'   )
+#'
+#' #Define the task function, which will be run in each time step.
+#' #You must pass 'agent' and 'population' as arguments to the function
+#' #so that the agent can see itself and other agents when it runs its task.
+#' move_and_report <- function(agent, population){
+#'
+#'   #Move the agent
+#'   agent$location_y <- agent$location_y + runif(1, -0.5, 0.5)
+#'   agent$location_x <- agent$location_x + runif(1, -0.5, 0.5)
+#'
+#'   #Print the agent's position to the console
+#'   print(
+#'     paste0("Agent '", agent$id, "' position: ",
+#'     agent$location_y, ", ", agent$location_x)
+#'   )
+#' }
+#'
+#' #Run the simulation with 10 time steps
+#' makePopulation(init_pop) %>%
+#'   setTask(move_and_report) %>%
+#'   agentSim(10) %>%
+#'   runSim()
+runSim <- function(x, debug = FALSE){
+  UseMethod("runSim", x)
+}
+
+#' @export
+runSim.simulation <- function(x, debug = FALSE){
+
+  pb = utils::txtProgressBar(min = 2, max = x$time_total, initial = 2)
+
+  for(t in 2:x$time_total){
+
+    if(debug) print(glue::glue("time: {t}"))
+
+    for(a in 1:length(x$agent_population)){
+      if(debug) print(glue::glue("agent: {a$id}"))
+
+      #Run task using t-1 parameters so that agents act simultaneously
+      x$agent_population[[a]] <-
+        x$agent_population[[a]]$task(agent = x$agent_population[[a]],
+                                     population = x$history$log[[t - 1]])
+    }
+
+    utils::setTxtProgressBar(pb, t)
+
+    #Store updated agent parameters
+    x$history$log[[t]] <- x$agent_population
+
+  }
+
+  x
+}
